e462437a9842938a6c68e7c3c5ed2d58cf9345e5
ditech <ditech@ditechnetworks.com>
2013-11-29 07:33:51 +0800

    Upload LUA

--- tilera/sde/components/pcicard/tilegx/callcap/runapp.sh
+++ tilera/sde/components/pcicard/tilegx/callcap/runapp.sh
@@ -8,7 +8,13 @@
 tile-monitor --hvx dataplane=4-255 \
   --hvx TLR_NETWORK=none --hvd POST=quick \
   --cd /root \
+  --upload-tile-libs lua \
+  --upload-tile-libs readline \
+  --upload-tile-libs ncurses \
+  --upload-tile-libs m \
+  --upload-tile-libs tinfo \
   --upload-tile-libs gxpci \
+  --upload-tile /usr/bin/lua \
   --upload callcap /root/callcap \
   --tile dataplane \
   -- callcap
