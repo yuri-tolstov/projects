588134a794e2ed7fbc26ec8fb25e3423e9770573
ditech <ditech@localhost.localdomain>
2014-01-12 09:53:16 -0800

    Update

--- oss/linux/drivers/tasklets/example1/example1.c
+++ oss/linux/drivers/tasklets/example1/example1.c
@@ -7,12 +7,24 @@ MODULE_LICENSE("GPL");
 char mydata[] = "My Tasklet function was called";
 
 /*Bottom-Half function*/
-void mytask_function(unsigned long data)
+void myfunc(unsigned long data)
 {
    printk("%s\n", (char *)data);
 }
 
-DECLARE_TASKLET(mytask, mytask_function, (unsigned long)&mydata);
+DECLARE_TASKLET(mytask, myfunc, (unsigned long)&mydata);
+/*
+ * DECLARE_TASKLET =
+ *    tasklet_struct mytask = {NULL, 0, ATOMIC_INIT(0), myfunc, mydata)
+ *
+ * struct tasklet_struct {
+ *    struct tasklet_struct *next;
+ *    ulong_t state;
+ *    atomic_t count;
+ *    void (*func)(ulong_t);
+ *    ulong_t data;
+ * };
+ */
 
 int init_module(void)
 {
@@ -20,6 +32,21 @@ int init_module(void)
    tasklet_schedule(&mytask);
    return 0;
 }
+/*
+ * tasklet_schedule(struct tasklet_struct *t)
+ * {
+ *    int cpu = smp_processor_id();
+ *    ulong_t flags;
+ *
+ *    local_irq_safe(flags);
+ *    t->next = tasklet_vec[cpu].list;
+ *    tasklet_vec[cpu].list = t;
+ *    __cpu_raise_softirq(cpu, TASKLET_SOFTIRQ);
+ *    local_irq_restore(flags);
+ * }
+ *
+ * struct tasklet_head tasklet_vec[NR_CPUS];
+ */
 
 void cleanup_module(void)
 {
