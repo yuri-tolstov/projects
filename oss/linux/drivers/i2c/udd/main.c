27b2c8866fc6dbcc37025f5fc5f463c6db43cabb
Yuri Tolstov <ytolstov@ytolstov-Lenovo-H530>
2014-02-24 08:29:05 -0800

    Update

--- oss/linux/drivers/i2c/udd/main.c
+++ oss/linux/drivers/i2c/udd/main.c
@@ -11,14 +11,12 @@
 #include <linux/i2c-dev.h>
 
 #define I2CDEV "/dev/i2c-1"
-#define I2CADDR 0x4D
+#define I2CADDR 0x54
 
 int main(int argc, char **argv, char **envp) {
    int c = 0; /*Return code*/
    int fd; /*File descriptor*/
-   int data; /*TODO: remove it*/
-   float volts; /*TODO: remove it*/
-   char buf[10] = {0}; /*Data buffer*/
+   char buf[8] = {0}; /*Data buffer*/
 
    /* Open the Device. */
    if ((fd = open(I2CDEV, O_RDWR)) < 0) {
@@ -36,24 +34,17 @@ int main(int argc, char **argv, char **envp) {
       goto main_fail_exit;
    }
    /* Send the reset command. */
-   while(1) {
-      buf[0] = 0x8c; //TODO:
-      if (write(fd, buf, 1) != 1) {
-         printf("write() failed, errno=%d\n", errno);
-      }
-      do {
-         if (read(fd, buf, 3) != 3) { //TODO:
-            printf("read() failed, errno=%d\n", errno);
-         }
-      } while (buf[2] & (1<<7));
-
-      data = (buf[0] << 8) | buf[1];
-      if (data > 32767) {
-         data = -(65535-data);
-      }
-      volts = ((float)data/32767)*2.048;
-      printf("data: %d volts: %.4f\n",data, volts);
+   buf[0] = 0x00; //TODO:
+   if (write(fd, buf, 1) != 1) {
+      printf("write(a=0x%x, d=0x%x) failed, errno=%d\n", I2CADDR, buf[0], errno);
+      goto main_fail_exit;
    }
+   if (read(fd, buf, 1) != 1) { //TODO:
+      printf("read(a=0x%x) failed, errno=%d\n", I2CADDR, errno);
+      goto main_fail_exit;
+   }
+   printf("data = 0x%x\n",buf[0]);
+
   /* Failure handler and exit. */
 main_fail_exit:
    close(fd);
