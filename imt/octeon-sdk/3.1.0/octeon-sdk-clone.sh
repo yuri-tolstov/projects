4bba80fd0ac7d4de6e0250a5206f77eb42f2d044
Yuri Tolstov <yurit@interfacemasters.com>
2014-06-07 11:51:53 -0700

    Update

--- imt/octeon-sdk/3.1.0/octeon-sdk-clone.sh
+++ imt/octeon-sdk/3.1.0/octeon-sdk-clone.sh
@@ -2,6 +2,23 @@
 #*******************************************************************************
 # Functions
 #*******************************************************************************
+function helpinfo() {
+echo "Usage:
+   octeon-sdk-clone.sh <sdk-set> <target-dir>
+Arguments:
+   sdk-set:
+      bootloader | executive | linux | docs | full
+   target-repo-url:
+      Target directory name (e.g. octeon-sdk.bootloader)
+Examples:
+   1. Install Full SDK in OCTEON-SDK directory.
+   $ ./octoen-sdk-clone.sh full OCTEON-SDK
+
+   2. Install Linux SDK in OCTEON-SDK.linux directory.
+   $ ./octoen-sdk-clone.sh linux OCTEON-SDK.linux
+"
+}
+
 function check_arguments() {
    local destdir=$1
    if test "$destdir" == ""; then
@@ -19,21 +36,21 @@ case $UCMD in
 #-------------------------------------------------------------------------------
    bootloader)
 #-------------------------------------------------------------------------------
-echo "Cloning OCETON SDK bootloader set..."
+echo "Cloning OCETON SDK bootloader Set..."
 SMODLIST="tools executive bootloader"
 CLONESDK=1
 ;;
 #-------------------------------------------------------------------------------
    executive)
 #-------------------------------------------------------------------------------
-echo "Cloning OCETON SDK executive set..."
+echo "Cloning OCETON SDK executive Set..."
 SMODLIST="tools executive"
 CLONESDK=2
 ;;
 #-------------------------------------------------------------------------------
    linux)
 #-------------------------------------------------------------------------------
-echo "Cloning OCETON SDK linux set..."
+echo "Cloning OCETON SDK linux Set..."
 SMODLIST="tools executive linux"
 CLONESDK=2
 ;;
@@ -41,34 +58,21 @@ CLONESDK=2
    docs)
 #-------------------------------------------------------------------------------
 check_arguments $TGTDIR
-echo "Cloning OCETON SDK docs set..."
+echo "Cloning OCETON SDK docs Set..."
 SMODLIST="docs"
 CLONESDK=3
 ;;
 #-------------------------------------------------------------------------------
    full)
 #-------------------------------------------------------------------------------
-echo "Cloning OCETON SDK full set..."
+echo "Cloning OCETON SDK full Set..."
 SMODLIST="tools executive bootloader linux docs"
 CLONESDK=4
 ;;
 #-------------------------------------------------------------------------------
    --help | *)
 #-------------------------------------------------------------------------------
-echo "Usage:
-   octeon-sdk-clone.sh <sdk-set> <target-dir>
-Arguments:
-   sdk-set:
-      bootloader | executive | linux | docs | full
-   target-repo-url:
-      Target directory name (e.g. octeon-sdk.bootloader)
-Examples:
-   1. Install Full SDK in OCTEON-SDK directory.
-   $ ./octoen-sdk-clone.sh full OCTEON-SDK
-
-   2. Install Linux SDK in OCTEON-SDK.linux directory.
-   $ ./octoen-sdk-clone.sh linux OCTEON-SDK.linux
-"
+helpinfo
 exit 0
 ;;
 esac
