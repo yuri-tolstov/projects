1120f9e4a4e860c90d05d7d973d9c8edf5dd1150
Yuri Tolstov <yurit@interfacemasters.com>
2014-08-15 10:44:52 -0700

    Update

--- imt/hbtool/release.sh
+++ imt/hbtool/release.sh
@@ -7,7 +7,7 @@ VERREL=$(awk '/HBTOOL_VERSION_RELNUM/ {print $3}' version.h)
 BLDNUM=$(awk '/HBTOOL_VERSION_BLDNUM/ {print $3}' version.h)
 RELARC=$RELNAME-$VERMAJ.$VERMIN.$VERREL-$BLDNUM.src.tgz
 
-FILIST="Makefile version.h niagara.h main.c n804.c n808.c"
+FILIST="Makefile version.h niagara.h main.c n804.c n808.c INSTALL"
 
 tar czvf $RELARC $FILIST > /dev/zero
 echo "Generated $RELARC"
