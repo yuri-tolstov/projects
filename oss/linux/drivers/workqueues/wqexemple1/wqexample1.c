8778fda2b6339f2137522c73c4b52169a5ac8867
Yuri Tolstov <yurit@interfacemasters.com>
2014-09-08 08:04:57 -0700

    Update

--- oss/linux/drivers/workqueues/wqexemple1/wqexample1.c
+++ oss/linux/drivers/workqueues/wqexemple1/wqexample1.c
@@ -11,6 +11,54 @@ typedef struct {
    int x;
 } mywork_t;
 
+#if 0
+================================================================================
+--------------------------------------------------------------------------------
+struct workqueue_struct {
+--------------------------------------------------------------------------------
+       struct list_head        pwqs;           /* WR: all pwqs of this wq */
+       struct list_head        list;           /* PL: list of all workqueues */
+       struct mutex            mutex;          /* protects this wq */
+       int                     work_color;     /* WQ: current work color */
+       int                     flush_color;    /* WQ: current flush color */
+       atomic_t                nr_pwqs_to_flush; /* flush in progress */
+       struct wq_flusher       *first_flusher; /* WQ: first flusher */
+       struct list_head        flusher_queue;  /* WQ: flush waiters */
+       struct list_head        flusher_overflow; /* WQ: flush overflow list */
+       struct list_head        maydays;        /* MD: pwqs requesting rescue */
+       struct worker           *rescuer;       /* I: rescue worker */
+       int                     nr_drainers;    /* WQ: drain in progress */
+       int                     saved_max_active; /* WQ: saved pwq max_active */
+       struct workqueue_attrs  *unbound_attrs; /* WQ: only for unbound wqs */
+       struct pool_workqueue   *dfl_pwq;       /* WQ: only for unbound wqs */
+#ifdef CONFIG_SYSFS
+       struct wq_device        *wq_dev;        /* I: for sysfs interface */
+#endif
+#ifdef CONFIG_LOCKDEP
+       struct lockdep_map      lockdep_map;
+#endif
+       char                    name[WQ_NAME_LEN]; /* I: workqueue name */
+       /* hot fields used during command issue, aligned to cacheline */
+       unsigned int            flags ____cacheline_aligned; /* WQ: WQ_* flags */
+       struct pool_workqueue __percpu *cpu_pwqs; /* I: per-cpu pwqs */
+       struct pool_workqueue __rcu *numa_pwq_tbl[]; /* FR: unbound pwqs indexed by node */
+};
+--------------------------------------------------------------------------------
+struct work_struct {
+--------------------------------------------------------------------------------
+       atomic_long_t data;
+       struct list_head entry;
+       work_func_t func;
+#ifdef CONFIG_LOCKDEP
+       struct lockdep_map lockdep_map;
+#endif
+};
+
+typedef void (*work_func_t)(struct work_struct *work);
+================================================================================
+#endif
+
+
 mywork_t *work, *work2;
 
 /*Bottom-Half Handler*/
