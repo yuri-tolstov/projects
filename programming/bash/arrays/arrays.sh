b9a0655fadd5887bbf75f4d7e68f10d7b03e9e18
Yuri Tolstov <yurit@interfacemasters.com>
2014-10-11 10:35:08 -0700

    Update

--- programming/bash/arrays/arrays.sh
+++ programming/bash/arrays/arrays.sh
@@ -12,7 +12,7 @@ echo "2: ${ARRAY[2]}"
 echo "3: ${ARRAY[3]}"
 
 echo "Scannig the array..."
-for ii in ${ADDRAY[@]}; do
+for ii in "${ARRAY[@]}"; do
    echo "$ii"
 done
 
@@ -24,10 +24,35 @@ AARRAY[key1]="value1"
 AARRAY[key2]="value2"
 AARRAY[key3]="value3"
 echo "All: ${AARRAY[@]}"
+
+echo "Scannig the a-array..."
 for ii in "${AARRAY[@]}"; do
    echo "$ii"
 done
+
 echo "Key1: ${AARRAY[key1]}"
 echo "Key2: ${AARRAY[key2]}"
 echo "Key3: ${AARRAY[key3]}"
+
+echo "Scannig the a-array different way..."
+KARRAY=(key1 key2 key3)
+for kk in "${KARRAY[@]}"; do
+   echo "$kk: ${AARRAY[$kk]}"
+done
+
+#-------------------------------------------------------------------------------
+# Practical example
+#-------------------------------------------------------------------------------
+echo "Practical example.."
+BOARDS=(n804 n805 n808)
+declare -A CPUS
+CPUS[n804]="CN6645"
+CPUS[n805]="CN6335"
+CPUS[n808]="CN6645"
+
+for ii in "${BOARDS[@]}"; do
+   echo "$ii: ${CPUS[$ii]}"
+done
+
+
 
